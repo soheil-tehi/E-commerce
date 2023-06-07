5df740afac394b8ec75802671eb7485aa5938d7e
Soroush Tehrani <soroush.art5@gmail.com>
2023-06-07 15:42:33 +0330

    FEATURE: Add Init Navbar UI

---
+++ src/Components/Navabar/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './logo.css'
+
+export default function Logo() {
+    return (
+        <div className='logoWrapper'>
+            <img src="images/logo.png" alt="logo" />
+        </div>
+    )
+}
--- src/Components/Navabar/Navbar.tsx
+++ src/Components/Navabar/Navbar.tsx
@@ -1,10 +1,132 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import { MdMenu, MdSearch, MdShoppingCart } from "react-icons/md";
+import Container from '@mui/material/Container';
+import MaterialButton from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Logo from './Logo/Logo';
+import Button from '../Button/Button';
+import './navbar.css';
 
-function Navbar() {
+const pages = ['صفحه اصلی', 'فرانت اند', 'امنیت', 'مقالات'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        // Please improve navbar
-        <></>
-    )
+        <AppBar className='bg-light' position="static">
+            <Container className='bg-light' maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <Logo />
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            className='textMainColor'
+                        >
+                            <MdMenu />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography style={{ width:'200px' }} className='textMainColor' textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <Logo />
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <MaterialButton
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ py: 2, display: 'block', fontSize: '1.2em', px: 3 }}
+                                className='textMainColor'
+                            >
+                                {page}
+                            </MaterialButton>
+                        ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <MaterialButton variant='contained' className='iconBtn searchBtn' >
+                            <MdSearch />
+                        </MaterialButton>
+                        <MaterialButton variant='contained' className='iconBtn ShoppingCartBtn me-2' >
+                            <MdShoppingCart />
+                        </MaterialButton>
+                        <Button variant='outlined' className='me-2'>
+                            سهیل طهرانی
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
-
-export default Navbar;
+export default ResponsiveAppBar;
