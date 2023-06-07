6f7c0f774e688025184fcf2fd301732ad3f7f33d
Soroush Tehrani <soroush.art5@gmail.com>
2023-06-07 15:41:53 +0330

    REFACTOR: Change Button Component Props

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,23 +3,18 @@ import MaterialButton from '@mui/material/Button';
 import './Button.css';
 
 interface ButtonProps {
-  href?: string;
-  isPreventDefault?: boolean;
   children?: ReactNode;
   variant: 'contained' | 'outlined';
   onClick?: (data?: any) => void;
-  target?: '_blank' | '_parent' | '_self' | '_top';
   className?: string;
   style?: CSSProperties;
 }
 
-function Button({ href, target, variant, className, children, ...otherBtnProps }: ButtonProps) {
+function Button({ variant, className, children, ...otherBtnProps }: ButtonProps) {
 
   return (
     <MaterialButton {...otherBtnProps} className={`customBtn ${variant === 'contained' ? 'containedBg' : 'outlinedBg'} ${className}`}>
-      <a href={href} target={target}>
         {children}
-      </a>
     </MaterialButton>
   );
 }
