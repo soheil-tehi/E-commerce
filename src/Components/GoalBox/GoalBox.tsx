7a005eeef62cbefe717d10bbc6258ca346d3c0c3
soheilst2 <soheil.st2990305@gmsil.com>
2023-06-09 00:02:41 +0330

    test stash

---
+++ src/Components/GoalBox/GoalBox.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function GoalBox() {
+    return (
+        <>
+            <div className='goal-box'>
+                <div className="goal-icon">
+                    <i></i>
+                </div>
+                <div className='goal-info'>
+                    <p>دوره پولی و رایگان</p>
+                    <p>براش مهم نیست. به مدرسینش حقوق میده تا نهایت کیفیت رو در پشتیبانی و اپدیت دوره ارائه بده</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GoalBox
