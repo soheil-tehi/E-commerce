eaf1413d651ffebc6a13a63f2666155c9e266d09
soheilst2 <soheil.st2990305@gmsil.com>
2023-06-15 12:34:21 +0330

    Create Login And Register Pages

--- src/Components/ArticlePage/MiniMenu/MiniMenuItem.tsx
+++ src/Components/ArticlePage/MiniMenu/MiniMenuItem.tsx
@@ -1,82 +1,8 @@
-// import React from 'react'
-// import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
-
-// import { Fragment, useEffect, useState } from 'react';
-
-// import "./MiniMenuItem.css"
-// import { Link } from 'react-router-dom';
-
-// interface MiniMenuItemProps {
-//     title: string;
-//     // allData: [{ id: number, title: string }]
-//     allData: { id: number; title: string; }[];
-//     coutsePath: string
-// }
-
-// function MiniMenuItem({ title, allData, coutsePath }: MiniMenuItemProps) {
-
-
-//     const [showSubMenu, setShowSubMenu] = useState(false);
-//     const [classSubmenu, setClassSubmenu] = useState("disableSub");
-
-//     function toggleShowSubMenu() {
-//         setShowSubMenu(!showSubMenu);
-//     }
-
-
-//     useEffect(() => {
-//         const toggleShow = showSubMenu ? "enableSub" : "disableSub";
-//         setClassSubmenu(toggleShow);
-//     }, [showSubMenu])
-
-//     return (
-//         <div className= 'mini-menu' >
-//         <ul>
-//         <li>
-//         <div className='mini-menu-item' >
-//             <p>{ title } < /p>
-//             < button onClick = { toggleShowSubMenu } >
-//                 {
-//                     showSubMenu?<AiOutlineArrowUp /> : <AiOutlineArrowDown />
-// }
-// </>
-//     < /div>
-// {
-//     showSubMenu &&
-//     <div className={ `mini-submenu ${classSubmenu}` }>
-//         <ul>
-
-//         {
-//             allData.map(item => (
-//                 <Fragment key= { item.id } >
-//                 <li>
-//                 <Link to={`/${coutsePath}/${item.id}`} > { item.title } < /Link>
-//         < /li>
-
-//         < /Fragment>
-//                                     ))
-// }
-
-// </ul>
-//     < /div>
-//                     }
-// </li>
-//     < /ul>
-//     < /div >
-
-//     );
-// }
-
-
-// export default MiniMenuItem;
 import React from 'react'
 import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
-
 import { Fragment, useEffect, useState } from 'react';
-
-
-import "./MiniMenuItem.css"
 import { Link } from 'react-router-dom';
+import "./MiniMenuItem.css"
 
 interface MiniMenuItemProps {
     title: string;
@@ -108,11 +34,11 @@ function MiniMenuItem({ title, allData, coursePath }: MiniMenuItemProps) {
                     <li>
                         <div className='mini-menu-item'>
                             <p>{title}</p>
-                            <button onClick={toggleShowSubMenu}>
+                            <a onClick={toggleShowSubMenu}>
                                 {
                                     showSubMenu ? <AiOutlineArrowUp /> : <AiOutlineArrowDown />
                                 }
-                            </button>
+                            </a>
                         </div>
                         {showSubMenu &&
                             <div className={`mini-submenu ${classSubmenu}`}>
